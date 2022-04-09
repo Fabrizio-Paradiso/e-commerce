7f5b65b059ef087a62c1d9aeb36efe27b00ec84d
Fabrizio Paradiso <fabrizioparadisom@gmail.com>
2022-04-09 12:52:09 -0300

    ItemDetail new styles

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,5 +1,3 @@
-import {AddIcon,MinusIcon} from "@chakra-ui/icons";
-
 const ItemCount = ({count, availableStock, handleCount}) => {
     
     const addition = () => ( count < availableStock ? handleCount(count+1) : null )  
@@ -7,17 +5,13 @@ const ItemCount = ({count, availableStock, handleCount}) => {
     const substract = () => ( count > 0 ? handleCount(count-1) : null )
 
     return (
-        <>
-        <div className="d-flex mx-auto justify-content-start" style={{paddingTop:"1.5rem"}}>
-            <button className="text-center py-auto" style={{all:"unset", cursor:"pointer", marginRight:"0.5rem", backgroundColor:"#F58A1F", width:"20px", height:"25px" , boxShadow: "0px 1px black", border:"0.06rem black solid"}} disabled={count===0} onClick={substract}>
-                <MinusIcon style={{fontSize:"0.9rem", fontWeight:"bold" ,color:"#fff"}}/>
-            </button>
-            <span className="my-auto">Quantity: {count}</span>
-            <button className="text-center p-auto" style={{all:"unset", cursor:"pointer",  marginLeft:"0.5rem", backgroundColor:"#F58A1F", width:"20px", height:"25px" , boxShadow: "0px 1px black", border:"0.06rem black solid"}} disabled={count===availableStock} onClick={addition}>
-                <AddIcon style={{fontSize:"0.9rem", fontWeight:"bold" ,color:"#fff"}}/>
+        <div style={{paddingTop:"1.5rem"}}>
+            <button value="1" type="button" class="d-flex justify-content-around align-items-center" style={{width:"70px", backgroundColor:"#F58A1F", boxShadow: "0px 1px black", border:"0.06rem black solid"}}>
+                <button style={{all:"unset"}} className="py-auto" disabled={count===0} onClick={substract}> <svg fill="#fff" stroke-width="0" filter= "drop-shadow(1.2px 1px 0.5px black)" viewBox="0 0 1024 1024" height="1rem" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M840.4 300H183.6c-19.7 0-30.7 20.8-18.5 35l328.4 380.8c9.4 10.9 27.5 10.9 37 0L858.9 335c12.2-14.2 1.2-35-18.5-35z"></path></svg> </button>
+                <p class="my-auto" style={{color:"#fff", fontWeight:"bold" , textShadow: "1.2px 1px 0.5px black" ,  padding:"0 0.2rem 0 0.2rem"}}>{count}</p>
+                <button style={{all:"unset"}} className="py-auto" disabled={count===availableStock} onClick={addition}> <svg fill="#fff" stroke-width="0" filter= "drop-shadow(1.2px 1px 0.5px black)" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M858.9 689L530.5 308.2c-9.4-10.9-27.5-10.9-37 0L165.1 689c-12.2 14.2-1.2 35 18.5 35h656.8c19.7 0 30.7-20.8 18.5-35z"></path></svg> </button> 
             </button>
         </div>
-        </>
     )
 }
 
--- src/components/ItemDetailContainer/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ItemCount from "../ItemCount/ItemCount"
 import dai from "../../imgs/icons/dai.png"
+import addToCart from "../../imgs/icons/addToCart.svg"
+import shopping from "../../imgs/icons/shopping.svg"
 import CartContextProvider, { useCartContext } from '../../context/CartContext'
 import { useState } from 'react'
 import {Link} from 'react-router-dom'
@@ -24,37 +26,40 @@ export const ItemDetail = ({item}) => {
             <img src={item.img} alt={item.name} style={{width: "400px", height: "400px" , padding:"50px", backgroundColor:"white"}}/>  
           </div>
         
-        <div className='item-detail-description d-flex-column col-4 text-center my-4' >
-          <h2 className='text-dark text-start' style={{fontWeight:"bolder", fontSize:"1.6rem"}}> {item.name} </h2>
-          <h5 className='text-start' style={{ paddingTop:"1.5rem", borderBottom: "1px solid rgb(212, 212, 212)"}}>Description</h5>
-          <span className='d-flex text-start'> {item.description} </span>
-          <span className='d-flex justify-content-start'style={{paddingTop:"1.5rem", fontWeight:"bold"}}>Price: {item.price}<img height={20} className="mx-2" alt="coin" src={dai} style={{marginBottom:"0.2rem"}}/></span>  
+        <div className='item-detail-description d-flex-column col-4 text-center my-1' >
+          <h1 className='text-dark text-start' style={{fontWeight:"bolder", fontSize:"2.3rem"}}> {item.name} </h1>
+          <h2 className='d-flex justify-content-start'style={{paddingTop:"0.2rem", fontSize:"1.7rem" , fontWeight:"bold", color:"black" , textShadow: "1.2px 1px 0.5px white"}}>{item.price}<img height={30} className="mx-2" alt="coin" src={dai} style={{marginTop:"0.1rem"}}/></h2>
+          <h3 className='d-flex text-start' style={{color:"black", fontSize:"1.1rem", paddingTop:"0.2rem"}}> Stock ({item.stock}) units </h3>
+
           <div className="d-flex-column text-start">
             {   
                 !clickAdd?
-                  <>
+                  <div className='d-flex' style={{marginTop:"0.1rem"}}>
                     <ItemCount count={currentCart} availableStock = {item.stock} handleCount={setCurrentCart}/>
-                    <span className='d-flex text-start' style={{color:"red", paddingTop:"1.5rem"}}> {item.stock} products in stock </span>
-                    <button className="text-center mx-auto px-auto" style={{all:"unset", marginTop:"1.5rem", cursor:"pointer", backgroundColor:"#F58A1F", width:"150px", height:"30px" , boxShadow: "0px 1px black", border:"0.06rem black solid"}} disabled={!currentCart} onClick={handleAddItem}>
-                      <span style={{fontSize:"1rem", fontWeight:"bold", borderBottom:"2rem" ,color:"#fff", textShadow: "1.2px 1px 0.5px black"}}>Add to cart</span>
+                    <button className="text-center px-auto justify-content-start" style={{all:"unset", margin:"1.5rem 1rem 0 1rem", cursor:"pointer", backgroundColor:"#F58A1F", width:"120px", boxShadow: "0px 1px black", border:"0.06rem black solid"}} disabled={!currentCart} onClick={handleAddItem}>
+                      <span style={{fontSize:"1rem", fontWeight:"bold", borderBottom:"2rem" ,color:"#fff", textShadow: "1.2px 1px 0.5px black"}}>Add to cart <img src={addToCart} alt="shop-bag" style={{height:"20px"}} ></img></span>
                     </button>
-                  </>
+                  </div>
                 :
-                    <>
+                    <div className='d-flex' style={{paddingTop:"0.1rem"}}>
                     <Link to={"/"} style={{textDecoration:'inherit', color:'inherit'}}>
-                      <button className="text-center px-auto" style={{all:"unset", marginTop:"2rem", marginRight:"1.5rem", cursor:"pointer", backgroundColor:"#F58A1F", width:"180px", height:"30px" , boxShadow: "0px 1px black",  border:"0.06rem black solid"}}>
-                            <span style={{fontSize:"1rem", fontWeight:"bold", color:"#fff", textShadow: "1.2px 1px 0.5px black"}}>Continue Shopping</span>
+                      <button className="text-center px-auto" style={{all:"unset", marginTop:"1rem", marginRight:"1.5rem", cursor:"pointer", backgroundColor:"#F58A1F", width:"200px", boxShadow: "0px 1px black",  border:"0.06rem black solid"}}>
+                            <span style={{fontSize:"1rem", fontWeight:"bold", color:"#fff", textShadow: "1.2px 1px 0.5px black"}}>Continue Shopping <img src={shopping} alt="shop-bag" style={{height:"22px", paddingBottom:"0.2rem"}} ></img> </span>
                       </button>
                     </Link>
                     <Link to={"/cart"} style={{textDecoration:'inherit', color:'inherit'}}>            
-                        <button className="text-center px-auto" style={{all:"unset", marginTop:"2rem", cursor:"pointer", backgroundColor:"#F58A1F", width:"180px", height:"30px" , boxShadow: "0px 1px black", border:"0.06rem black solid"}}>
-                          <span style={{fontSize:"1rem", fontWeight:"bold", color:"#fff", textShadow: "1.2px 1px 0.5px black"}}>Go to cart</span>
+                        <button className="text-center px-auto" style={{all:"unset", marginTop:"1rem", cursor:"pointer", backgroundColor:"#F58A1F", width:"160px", boxShadow: "0px 1px black", border:"0.06rem black solid"}}>
+                          <span style={{fontSize:"1rem", fontWeight:"bold", color:"#fff", textShadow: "1.2px 1px 0.5px black"}}> Shopping Cart <img src={addToCart} alt="shop-bag" style={{height:"20px"}} ></img> </span>
                         </button>
                     </Link>
-                    </>
+                    </div>
             }
 
         </div>
+
+          <h5 className='text-start' style={{ paddingTop:"1.2rem", borderBottom: "1px solid rgb(212, 212, 212)"}}>Description</h5>
+          <span className='d-flex text-start'> {item.description} </span>
+
         </div>
       </div>
     </>

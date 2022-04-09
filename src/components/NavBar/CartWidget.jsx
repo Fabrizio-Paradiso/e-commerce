49dfd6caa52075da94fc6158696ee64e23ef4d98
Fabrizio Paradiso <fabrizioparadisom@gmail.com>
2022-04-09 09:53:50 -0300

    NavBar new styles

--- src/components/NavBar/CartWidget.jsx
+++ src/components/NavBar/CartWidget.jsx
@@ -6,16 +6,17 @@ export const CartWidget = () => {
     const { cartQuantity } = useCartContext(CartContextProvider);
     
     return (
-            <>
+            <div style={{marginRight:"3rem"}}>
               <img
                 width="60px"
                 height="35px"
                 src= {cart}
                 target="_blank" 
-                alt="CartIcon">
+                alt="CartIcon"
+              >
               </img>
               <Badge className="text-center" style={{color:"#fff", backgroundColor:"#F58A1F", borderRadius:"8rem", borderColor:"black", width:"22px", height:"22px", fontSize:"0.9rem",  marginBottom:"1rem", marginLeft:"-0.6rem", textShadow: "1.2px 1px 0.5px black",  filter:"drop-shadow(1.2px 1px 0.5px black"}}> <span className='text-center'>{cartQuantity}</span> </Badge>  
-            </>
+            </div>
     )
   }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,5 @@
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import Container from 'react-bootstrap/Container'
 import logo from '../../imgs/icons/logo.svg'
 import { CartWidget } from './CartWidget'
 import {NavLink} from 'react-router-dom'
@@ -9,41 +8,39 @@ import CartContextProvider, { useCartContext } from '../../context/CartContext'
 function NavBar () {
     const { cartList } = useCartContext(CartContextProvider)
     return (
-      <Navbar collapseOnSelect expand="lg" style={{backgroundImage:"url('https://www.digitalsport.com.ar/files/stores/skin_fluid.png')", boxShadow:"0 5px 2px -2px #9c9c9c"}}>
-        <Container>
-          <NavLink to="/">
-            <Navbar.Brand>
+      <Navbar collapseOnSelect expand="lg" style={{backgroundImage:"url('https://www.digitalsport.com.ar/files/stores/skin_fluid.png')", backgroundSize:"cover", boxShadow:"0 5px 2px -2px #9c9c9c"}}>          
+        <NavLink to="/">
+            <Navbar.Brand >
               <img
                 src={logo}
                 width="100"
                 height="50"
-                className="d-inline-block align-top text-start"
-                alt="React Bootstrap logo"
+                style={{display:"inline-block", position:"relative", float:"left", marginLeft:"3rem"}}
+                alt="Rosario Store logo"
               />
             </Navbar.Brand>
           </NavLink>
 
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mx-auto text-center">
-                <NavLink to="/category/accesories" style={{textDecoration:'inherit', color:'#f58a1f', fontWeight:"bolder", padding:"0px 10px 0px 10px", textShadow: "1.2px 1px 0.5px black"}} >Accesories</NavLink>
-                <NavLink to="/category/clothing" style={{textDecoration:'inherit', color:'#f58a1f', fontWeight:"bolder", padding:"0px 10px 0px 10px", textShadow: "1.2px 1px 0.5px black"}} >Clothing</NavLink>
-                <NavLink to="/category/shoes"  style={{textDecoration:'inherit', color:'#f58a1f', fontWeight:"bolder", padding:"0px 10px 0px 10px", textShadow: "1.2px 1px 0.5px black"}}>Shoes</NavLink>
-              </Nav>
-              <Nav>
-                <NavLink to="/cart" className="d-none d-md-block">
-                  <>
-                    {
-                      cartList.length?
-                        <CartWidget/>
-                        :
-                        null
-                    }
-                  </>
-                </NavLink>
-              </Nav>
-          </Navbar.Collapse>
-        </Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto text-center">
+              <NavLink to="/category/accesories" style={{textDecoration:'inherit', color:'#f58a1f', fontWeight:"bolder", padding:"0 3.9rem 0 3rem", textShadow: "1.2px 1px 0.5px black"}} >Accesories</NavLink>
+              <NavLink to="/category/clothing" style={{textDecoration:'inherit', color:'#f58a1f', fontWeight:"bolder", padding:"0 3.9rem 0 3rem", textShadow: "1.2px 1px 0.5px black"}} >Clothing</NavLink>
+              <NavLink to="/category/shoes"  style={{textDecoration:'inherit', color:'#f58a1f', fontWeight:"bolder", padding:"0 3.9rem 0 3rem", textShadow: "1.2px 1px 0.5px black"}}>Shoes</NavLink>
+            </Nav>
+            <Nav>
+              <NavLink to="/cart" className="d-none d-md-block">
+                <>
+                  {
+                    cartList.length?
+                      <CartWidget/>
+                      :
+                      null
+                  }
+                </>
+              </NavLink>
+            </Nav>
+        </Navbar.Collapse>
       </Navbar>
     )
 }
